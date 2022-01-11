6c22187c3cc28d15893f064c05b5d8f2bfdc6c80
Filipp Trubin <55filipp@gmail.com>
2022-01-11 17:21:10 -0500

    010

--- script.R
+++ script.R
@@ -1,77 +1,95 @@
-library(ggplot2)
 library(dplyr)
-library(esquisse)
+library(ggplot2)
 library(lubridate)
-library(zoo)
-options(scipen = 999)
 
+#Load dataset "Indicators of Anxiety or Depression Based on Reported Frequency of Symptoms During Last 7 Days" (https://data.cdc.gov):
+#[Dataset description](https://data.cdc.gov/NCHS/Indicators-of-Anxiety-or-Depression-Based-on-Repor/8pt5-q6wp)
+data_survey <- read.csv("https://data.cdc.gov/api/views/8pt5-q6wp/rows.csv?accessType=DOWNLOAD") 
+head(data_survey)
 
-data <- read.csv('data/Indicators_of_Anxiety_or_Depression_Based_on_Reported_Frequency_of_Symptoms_During_Last_7_Days.csv')
-
-data_w1 <- data[which(data$Indicator == 'Symptoms of Depressive Disorder'
-          & data$Group == 'By Sex'
-          & data$Subgroup == 'Female'
+#Filter down to focus group: Var.1: "Depression Value" Var.2: "Female"
+data_survey_1 <- data_survey[which(data_survey$Indicator == 'Symptoms of Depressive Disorder'
+          & data_survey$Group == 'By Sex'
+          & data_survey$Subgroup == 'Female'
           ),]
 
-data_w2 <- data_w1 %>% dplyr::select(Time.Period.End.Date, Value)
+#Convert Date field / drop NA
+data_survey_2 <- data_survey_1 %>% dplyr::select(Time.Period.End.Date, Value)
+data_survey_2$Time.Period.End.Date <- mdy(data_survey_2$Time.Period.End.Date)
+data_survey_3 <- data_survey_2[complete.cases(data_survey_2), ]
 
-data_w2$Time.Period.End.Date <- mdy(data_w2$Time.Period.End.Date)
-#data_w2$Date <- as.Date(data_w2$Time.Period.End.Date, format='%m/%d/%y')
+ggplot(data_survey_3) +
+  aes(x = Time.Period.End.Date, y = Value, colour = Value) +
+  geom_line(size = 1L) +
+  scale_color_distiller(palette = 'Reds', direction = 1) +
+  labs(
+    title = 'Depression Value per day',
+    x = NULL,
+    y = 'Depression Value'
+  ) +
+  theme_classic() +
+  ylim(0, 40)
 
-data_w3 <- data_w2[complete.cases(data_w2), ]
 
+#Load dataset "United States COVID-19 Cases and Deaths by State over Time" (https://data.cdc.gov):
+#[Dataset description](https://data.cdc.gov/Case-Surveillance/United-States-COVID-19-Cases-and-Deaths-by-State-o/9mfq-cb36)
+data_COVID19 <- read.csv("https://data.cdc.gov/api/views/9mfq-cb36/rows.csv?accessType=DOWNLOAD")
+head(data_survey)
 
-data_1 <- read.csv('data/United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv')
+#Filter down focus group: Var.1: "Date" Var.2: "Number of cases"
+data_COVID19_2 <- data_COVID19 %>% dplyr::select(submission_date, new_case)
+data_COVID19_2$submission_date <- mdy(data_COVID19_2$submission_date)
+data_COVID19_2 <- data_COVID19_2 %>% filter(new_case > 0 & submission_date <= as.Date('2021-12-13'))
 
-data_2 <- data_1 %>% dplyr::select(submission_date, new_case)
-data_2 <- data_2 %>%
-  filter(new_case > 0)
-
-data_2$submission_date <- mdy(data_2$submission_date)
-
-data_3 <- data_2 %>%
+#Grouping number of cases by day
+data_COVID19_3 <- data_COVID19_2 %>%
   group_by(submission_date) %>%
   summarize(new_case_daily = sum(new_case, na.rm = TRUE))
 
-data_joined <- merge(x = data_w3, y = data_3, by.x=c('Time.Period.End.Date'), by.y=c('submission_date'))
+ggplot(data_COVID19_3) +
+  aes(x = submission_date, y = new_case_daily, colour = new_case_daily) +
+  geom_line(size = 1L) +
+  scale_color_distiller(palette = "Blues", direction = 1) +
+  labs(
+    title = "COVID19 cases per day",
+    x = NULL,
+    y = 'COVID-19 Cases'
+  ) +
+  ylim(0, 700000) +
+  scale_x_date(limit=c(as.Date('2020-06-01'), as.Date('2021-12-13'))) +
+  theme_classic() 
 
-# data_3 <- data_3 %>%
-#     dplyr::arrange(desc(submission_date)) %>%
-#     dplyr::mutate(submission_date_7da = zoo::rollmean(data_3$conf_cases, k = 7, fill = NA))
 
-cor(data_joined[1:20, 2], data_joined[1:20, 3])
+#Merge datasets. Number of cases and Depression Value aggregated by day   
+data_joined <- merge(x = data_survey_3, y = data_COVID19_3, by.x=c('Time.Period.End.Date'), by.y=c('submission_date'))
+
+#Add new column: rolling Correlation coefficient (Pearson method)
 data_joined[ , 'Correlation_Coef'] <- NA
 for (i in 1:nrow(data_joined)) {
   data_joined[i, 4] = as.numeric(cor(data_joined[1:i, 2], data_joined[1:i, 3]))
 }
 data_joined[1, 4] = 0
-data_joined$Correlation_Coef <- format(round(data_joined$Correlation_Coef, 2), nsmall = 2)
-#esquisse::esquisser()
+data_joined$Correlation_Coef <- round(data_joined$Correlation_Coef, digits = 2)
 
 
- ggplot() +
- aes(x = Time.Period.End.Date, y = Correlation_Coef, fill = Value, colour = Correlation_Coef, 
- size = Correlation_Coef) +
- geom_point(shape = "circle") +
- scale_fill_distiller(palette = "YlGn", 
- direction = 1) +
- scale_color_distiller(palette = "YlGn", direction = 1) +
- theme_classic() +
- theme(legend.position = "none")
 
-
+#Combine graphs for EDA: Depression Value + COVID19 cases + rolling Corr. coef.
 ggplot(data = data_joined) +
-  geom_line(aes(x = Time.Period.End.Date, y = new_case_daily / 10000, color = 'blue')) +
-  geom_line(aes(x = Time.Period.End.Date, y = Value, color = 'red')) +
-  aes(x = Time.Period.End.Date, y = Correlation_Coef * 10 + 50, fill = Correlation_Coef, colour = as.factor(Correlation_Coef)) +
+  geom_line(aes(x = Time.Period.End.Date, y = new_case_daily / 10000), color='blue') +
+  geom_line(aes(x = Time.Period.End.Date, y = Value), color='red') +
+  aes(x = Time.Period.End.Date, y = Correlation_Coef * 10 + 50, color = Correlation_Coef) +
   geom_point(shape = 'circle') +
-  # scale_fill_distiller(palette = "YlGn", direction = 1) +
-  # scale_color_distiller(palette = "YlGn", direction = 1) +
-
   lims(y = c(0, 100)) +
-  scale_x_date(limit=c(as.Date("2020-05-05"), as.Date("2021-12-13")),
-               date_breaks = "3 month",
-               date_labels = "%Y/%m" ) +
+  scale_x_date(limit=c(as.Date('2020-06-01'), as.Date('2021-12-13')),
+               date_breaks = '3 month',
+               date_labels = '%Y/%m' ) +
+  labs(x = NULL, y = NULL, title = 'COVID-19 cases | Depression Value | Rolling Correlation coef') +
   theme_classic()
-  
+
+#Build LR model
+linear_regression = lm(Value ~ new_case_daily, data = data_joined)
+
+#P-value (0.04814) is less than 0.05 which means the relationship is statistically significant
+#and indicates strong evidence against the null hypothesis.
+summary(linear_regression)
 
